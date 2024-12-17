bb0664bc9190bbf99f90cafe72a88c0af9939f43
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-17 13:41:55 +0100

    Add project files.

---
+++ looper 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace loopar_5
+
+{
+    class program
+    {
+        static void Main(string[] args)
+              
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                if (i % 2 ==0)
+                {
+                    Console.WriteLine(i);
+
+
+                }
+
+
+            }
+
+        }
+    }
+}
